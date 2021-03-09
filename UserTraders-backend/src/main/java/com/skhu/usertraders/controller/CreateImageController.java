7cf88bd999d995e3402a21df28bdcb1aed52867a
jhj960918 <jhj960918@gmail.com>
2021-03-09 19:39:03 +0900

    다중 파일 업로드

---
+++ UserTraders-backend/src/main/java/com/skhu/usertraders/controller/CreateImageController.java
@@ -0,0 +1,35 @@
+package com.skhu.usertraders.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping(value = "/api")
+
+@Slf4j
+public class CreateImageController {
+
+    @ResponseBody
+    @ResponseStatus(value = HttpStatus.OK)
+    @RequestMapping(method = RequestMethod.POST, path = "/image")
+    public String fileupload(@RequestParam Map params, @RequestBody List<MultipartFile> files, @RequestParam(value = "keyword", required = false, defaultValue = "") String keyword) {
+        log.info(params.toString());
+        try {
+            for (int i = 0; i < files.size(); i++) {
+                files.get(i).transferTo(new File("C:\\SKHU-project\\IC-Capstone-User-Traders\\UserTraders-backend\\src\\main\\resources\\static\\images\\" + files.get(i).getOriginalFilename()));
+                log.info("완료?");
+            }
+        } catch (IllegalStateException | IOException e) {
+            e.printStackTrace();
+        }
+        return "file upload";
+    }
+
+}
