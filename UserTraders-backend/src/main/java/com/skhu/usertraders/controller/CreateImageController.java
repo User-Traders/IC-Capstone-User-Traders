805f009e38fed02b0db5df6c0f76574138cda324
jhj960918 <jhj960918@gmail.com>
2021-03-10 15:08:18 +0900

    front merge

--- UserTraders-backend/src/main/java/com/skhu/usertraders/controller/CreateImageController.java
+++ UserTraders-backend/src/main/java/com/skhu/usertraders/controller/CreateImageController.java
@@ -1,30 +1,25 @@
 package com.skhu.usertraders.controller;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping(value = "/api")
 
-@Slf4j
 public class CreateImageController {
+    static final String filePath = "C:\\SKHU-project\\IC-Capstone-User-Traders\\UserTraders-backend\\src\\main\\resources\\static\\images\\"; //파일 명
 
     @ResponseBody
     @ResponseStatus(value = HttpStatus.OK)
     @RequestMapping(method = RequestMethod.POST, path = "/image")
-    public String fileupload(@RequestParam Map params, @RequestBody List<MultipartFile> files, @RequestParam(value = "keyword", required = false, defaultValue = "") String keyword) {
-        log.info(params.toString());
+    public String fileupload(@RequestBody List<MultipartFile> files) {
         try {
             for (int i = 0; i < files.size(); i++) {
-                files.get(i).transferTo(new File("C:\\SKHU-project\\IC-Capstone-User-Traders\\UserTraders-backend\\src\\main\\resources\\static\\images\\" + files.get(i).getOriginalFilename()));
-                log.info("완료?");
+                files.get(i).transferTo(new File(filePath + files.get(i).getOriginalFilename()));
             }
         } catch (IllegalStateException | IOException e) {
             e.printStackTrace();
