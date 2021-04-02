2501991c9b71d237acf73e6698cbb10ca40fd323
jhj960918 <jhj960918@gmail.com>
2021-04-02 18:02:50 +0900

    hj frontend = > domain signup , depart list  add

--- UserTraders-frontend/src/utils/http/domain.js
+++ UserTraders-frontend/src/utils/http/domain.js
@@ -7,5 +7,7 @@ export default {
     'update': { task: 'put', url: '/users/#id' },
     'delete':{ task: 'delete', url: '/users/#id' },
     'login':{ task: 'post', url: '/login/#id' },
+    'listdepartment':{task:'get',url:'/departments/list'},
+    'signup': {task:'post',url:'/users/signup'},
   }
 }
