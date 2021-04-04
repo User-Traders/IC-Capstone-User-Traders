a8d2069ed452a47aa5ace747394536d7000f2dc1
jhj960918 <jhj960918@gmail.com>
2021-04-04 22:55:10 +0900

    login 시 token 처리

--- UserTraders-frontend/src/store/modules/users.js
+++ UserTraders-frontend/src/store/modules/users.js
@@ -38,12 +38,37 @@ const actions = {
     console.log(loginObj)
     return http.process("user", "login", loginObj
     ).then((res) => {
+      //성공시 토큰 들어옴
+      //토클을 헤더에 포함시켜 유저 정보 요청
       commit("setLoginTokken", res);
 
-    }).catch(err => {
-      console.log(err)
-      commit("loginError")
-    });
+      // let config = {
+      //   headers: {
+      //     "access-tokken": res,
+      //   }
+      // }
+      // http.process("user", "userinfo", res)
+      //   .then(res => { console.log(res) })
+      //   .catch(err => { console.log(err) })
+    })
+
+
+      .catch(err => {
+        console.log(err)
+        commit("logoutState")
+      });
+  },
+  getUserLogout({commit}){
+    if (confirm("로그아웃 하실거에요?")) {
+      return http.process("user", "logout")
+        .then((res) => {
+          console.log(res)
+          commit("logoutState")
+          this.$router.push({ name: 'Home1' });
+
+        }).catch((err) => { console.log(err) })
+
+    }
   },
 
   login({ state, commit }, loginObj) {
@@ -61,18 +86,25 @@ const actions = {
 };
 
 const mutations = {
-  //로그인 성공 했을 때
-  loginSuccess(state) {
+  //로그인 성공
+  setLoginTokken(state, data) {
     state.isLogin = true
     state.isLoginError = false
+    state.loginTokken = data
+    alert("Complete Login")
     router.push({ name: 'Home1' })
-
   },
 
   //로그인 실패
   loginError(state) {
     state.isLogin = false
     state.isLoginError = true
+
+  },
+  //로그아웃 처리 상태값 변환
+  logoutState(state) {
+    state.isLogin = false
+    state.isLoginError = false
 
   },
 
@@ -92,12 +124,7 @@ const mutations = {
     state.listDataDeatail = data;
 
   },
-  setLoginTokken(state, data) {
-    console.log(data)
-    state.loginTokken = data
-    alert("Complete Login")
-    router.push({ name: 'Home1' })
-  }
+ 
 };
 
 export default {
