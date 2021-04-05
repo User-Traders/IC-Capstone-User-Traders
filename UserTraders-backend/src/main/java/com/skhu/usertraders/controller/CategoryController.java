fc9060a17b465c2c7926e038d5ea160c2b891b99
jhj960918 <jhj960918@gmail.com>
2021-04-05 23:51:14 +0900

    no message

--- UserTraders-backend/src/main/java/com/skhu/usertraders/controller/CategoryController.java
+++ UserTraders-backend/src/main/java/com/skhu/usertraders/controller/CategoryController.java
@@ -4,6 +4,7 @@ import com.skhu.usertraders.service.CategoryService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,15 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
+@CrossOrigin(origins = "http://localhost:8080")
 @RequestMapping(value = "/categories")
 public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
-    @GetMapping(value = "/list/board"
-    ) // 모든 유저의 학과 리스트 반환
+    @GetMapping(value = "/list/board")
     public ResponseEntity list_board() {
-
         return ResponseEntity.ok(categoryService.findAll());
     }
 
