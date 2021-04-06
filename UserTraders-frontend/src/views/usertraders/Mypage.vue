52c0726d13925f0496032ca24be3515f805d6abf
jhj960918 <jhj960918@gmail.com>
2021-04-06 18:55:40 +0900

    mypage

---
+++ UserTraders-frontend/src/views/usertraders/Mypage.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>{{userInfo}}
+    {{userBList}}
+  </div>
+
+</template>
+<script>
+import { mapState } from "vuex";
+import http from "@/utils/http";
+export default {
+  data() {
+    return {
+      userBList: [],
+    }
+  },
+
+  mounted() {
+    if (localStorage.getItem("user")) {
+      const token = localStorage.getItem("user")
+      this.userBoardList(token)
+    }
+
+  },
+  computed: {
+    ...mapState({
+      userInfo: (state) => state.users.userInfo,
+
+    }),
+  },
+  methods: {
+    userBoardList(token) {
+      console.log(token)
+      return http.process("user", "userBoard", null, { token: token }).then((res) => {
+        console.log(res)
+        this.userBList = res
+      }).catch((err) => {
+        console.log(err)
+      })
+    },
+  },
+}
+</script>
