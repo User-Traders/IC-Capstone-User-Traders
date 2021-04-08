8be1abedc43d37890099979f4ca0a373e4f4877f
jhj960918 <jhj960918@gmail.com>
2021-04-08 19:38:20 +0900

    tel 필드 추가 숫자만 입력 기능하도록 regrex pattern 적용

--- UserTraders-frontend/src/views/usertraders/SignUp.vue
+++ UserTraders-frontend/src/views/usertraders/SignUp.vue
@@ -1,6 +1,7 @@
 <template>
 
   <div>
+    <br>
     <v-card width="400" class="mx-auto ">
       <v-card-title style="background-color: lightgrey; margin-bottom:5px;">
         <h3 style="color: white">
@@ -36,7 +37,12 @@
               </v-row>
               <v-row>
                 <v-col>
-                  <v-select v-model="select" :items="items" @click="departmentName" label="학과 선택" data-vv-name="select" required outlined></v-select>
+                  <v-text-field v-model="number" label="휴대폰 번호 -없이 입력" prepend-icon="mdi-cellphone" oninput="javascript: this.value = this.value.replace(/[^0-9]/g, '');" outlined></v-text-field>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col>
+                  <v-select v-model="select" :items="items" @click="departmentName" label="학과 선택" prepend-icon="mdi-school" data-vv-name="select" required outlined></v-select>
                 </v-col>
               </v-row>
             </v-container>
@@ -74,14 +80,21 @@ export default {
       items: [],
       itemsid: [],
       select: "",
+      number: "",
       rules: {
         required: input => !!input || "Required.",
         minPw: password => password.length >= 8 || "Min 8 characters",
-        minName: Name => Name.length >= 6 || "Min 6 characters",
+        minName: Name => Name.length >= 1 || "Min 1 characters",
+        minPhone: number => number.length >= 11 || "Min 11 characters",
         pwCheck: passwordCheck =>
           this.password === passwordCheck || "Password mismatch"
       }
     };
+  },
+  watch: {
+    number: function () {
+      return this.number = this.number.replace(/[^0-9]/g, '');
+    }
   },
   methods: {
     departmentName() {
@@ -98,11 +111,11 @@ export default {
     signUpSubmit() {
       const temp = this.select.split(" ")
       const department = { id: temp[0] }
-      const userObj = { userid: this.email, password: this.password, department: department, name: this.name }
+      const userObj = { userid: this.email, password: this.password, tel: this.number, department: department, name: this.name }
       return http.process("user", "signup", userObj)
         .then((res) => {
           console.log(res)
-          this.$router.push({ name: 'Home1' });
+          this.$router.push({ name: 'UserLogin' });
 
         }).catch((err) => { console.log(err) })
 
