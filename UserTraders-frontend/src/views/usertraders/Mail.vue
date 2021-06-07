4c6a4f02ccdf968e2a65d07c5f535168775fa634
jaebin1234 <jaebin2586@gmail.com>
2021-06-07 18:10:29 +0900

    최종 배포 수정

--- UserTraders-frontend/src/views/usertraders/Mail.vue
+++ UserTraders-frontend/src/views/usertraders/Mail.vue
@@ -1,67 +1,64 @@
 <template>
-  <v-simple-table>
+  <v-sheet height="600" class="overflow-hidden" style="position: relative;">
+    <v-container class="fill-height">
+      <v-row align="center" justify="center">
+        <v-btn color="#F57C00" dark @click.stop="drawer = !drawer">
+          쪽지함
+        </v-btn>
+      </v-row>
+    </v-container>
 
-    <template v-slot:default>
-      <thead>
-        <tr>
-          <th class="text-left">
-            내용
-          </th>
-          <th class="text-left">
-            보낸사람
-          </th>
-          <th>읽음</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="item in mail" :key="item.id">
-          <td>{{ item.title }}</td>
-          <td>{{ item.recvId.username }}</td>
-          <td>{{ item.recvRead }}</td>
-          <td>
-            <v-btn color="#F57C00" style="color:white" @click="messageSend(item.sentId.id,item.sentId.username)">
-              답장하기
-            </v-btn>
-          </td>
-        </tr>
-
-      </tbody>
-    </template>
-  </v-simple-table>
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list-item>
+        <v-list-item-avatar>
+          <v-img src="https://image.bugsm.co.kr/artist/images/1000/800491/80049126.jpg"></v-img>
+        </v-list-item-avatar>
+        <v-list-item-content>
+          <v-list-item-title>사용자</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <v-divider></v-divider>
+      <v-list dense>
+        <v-list-item :to="{ name: 'MailWrite'}" active-class="deep-purple--text text--accent-4">
+          <v-list-item-icon>
+            <v-icon>mdi-email-plus-outline</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>쪽지쓰기</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item :to="{ name: 'MailGet'}" active-class="deep-purple--text text--accent-4">
+          <v-list-item-icon>
+            <v-icon>mdi-email-send-outline</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>받은쪽지함</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item :to="{ name: 'MailSend'}" active-class="deep-purple--text text--accent-4">
+          <v-list-item-icon>
+            <v-icon>mdi-email-receive-outline</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>보낸쪽지함</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </v-sheet>
 </template>
 <script>
-import http from "@/utils/http";
 export default {
   data() {
     return {
-      mail: [],
-      token: "",
-    };
+      drawer: null,
+      // items: [
+      //   { title: '쪽지쓰기', icon: 'mdi-email-plus-outline', link: 'MailWrite'  },
+      //   { title: '받은쪽지함', icon: 'mdi-email-send-outline' },
+      //   { title: '보낸쪽지함', icon: 'mdi-email-receive-outline' },
+      //   { title: '휴지통', icon: 'mdi-delete-forever' },
+      // ],
+    }
   },
-
-  mounted() {
-    this.init();
-  },
-  methods: {
-    init() {
-      this.token = localStorage.getItem("user");
-      this.messageGet()
-    },
-    messageGet() {
-      return http
-        .process("mail", "recv", {}, { token: this.token })
-        .then((res) => {
-          console.log(res)
-          this.mail = res
-        })
-        .catch((err) => {
-          console.log(err)
-        });
-    },
-    messageSend(id, username) {
-      this.$router.push({ name: "MailWrite", params: { id: id, email: username } });
-    },
-
-  },
-};
+}
 </script>
